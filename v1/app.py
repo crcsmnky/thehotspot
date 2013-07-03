c31e9c0a81a847a5eae0f3ac85c91deddcda5376
Sandeep Parikh <sandeep@clusterbeep.org>
2013-07-02 23:50:35 -0700

    Using different port numbers for each app version

--- v1/app.py
+++ v1/app.py
@@ -98,5 +98,5 @@ def newcheckin():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
--- v3/app.py
+++ v3/app.py
@@ -132,5 +132,5 @@ def category(catid):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5003))
     app.run(host='0.0.0.0', port=port)
